5e4ad08a93ce216560da21d362f1bed78fa0e9f7
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-11-02 18:33:28 +0100

    added hashing for parsed passwords

--- test/initialPwd.py
+++ test/initialPwd.py
@@ -1,14 +1,19 @@
 from csv import DictReader
 from json import dump
 
+from bcrypt import hashpw, gensalt
+
 csv_file_path = "test/patients.csv"
 json_file_path = "test/initialPwd.json"
 initial_pwd = {}
 
+def mhash(plain: str, salt: bytes = gensalt()) -> str:
+    return hashpw(plain.encode(), salt).decode()
+
 with open(csv_file_path, "r") as filestream:
     patients_data = DictReader(filestream)
     for row in patients_data:   
-        initial_pwd[row["Patient"]] = row["ID/Passwort"]
+        initial_pwd[row["Patient"]] = mhash(row["ID/Passwort"])
     
 with open(json_file_path, "w") as filestream:
     dump(initial_pwd, filestream, indent=4, ensure_ascii=False)
