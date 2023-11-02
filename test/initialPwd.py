6b382983a61e206bd3ceb522b480b2a038e9e4e2
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-02 23:27:34 +0100

    adding missing .decode()

--- test/initialPwd.py
+++ test/initialPwd.py
@@ -5,5 +5,5 @@ original_file_path = "patients.csv"
 new_file_path = "pwd.csv"
 
 df = read_csv(original_file_path, usecols=["Patient", "ID/Passwort"])
-df["Hash"] = df["ID/Passwort"].map(lambda plain: hashpw(plain.encode(), gensalt()))
+df["Hash"] = df["ID/Passwort"].map(lambda plain: hashpw(plain.encode(), gensalt()).decode())
 df.to_csv(new_file_path, columns=["Patient", "Hash"], index=False)
