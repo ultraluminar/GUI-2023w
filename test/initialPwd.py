a9bd262dfb018c1c25a9e06d0510696a61ee21e7
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-11-02 18:16:34 +0100

    added parsing from the patients csv file to a pwd json

---
+++ test/initialPwd.py
@@ -0,0 +1,14 @@
+from csv import DictReader
+from json import dump
+
+csv_file_path = "test/patients.csv"
+json_file_path = "test/initialPwd.json"
+initial_pwd = {}
+
+with open(csv_file_path, "r") as filestream:
+    patients_data = DictReader(filestream)
+    for row in patients_data:   
+        initial_pwd[row["Patient"]] = row["ID/Passwort"]
+    
+with open(json_file_path, "w") as filestream:
+    dump(initial_pwd, filestream, indent=4, ensure_ascii=False)
