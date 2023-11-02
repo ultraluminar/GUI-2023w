60906ba07bea303334ee7a5eb0e204d3044e5fc9
KlasL4243 <lukasklassen4243@gmail.com>
2023-11-02 23:18:49 +0100

    initialPwd.py using pandas

--- test/initialPwd.py
+++ test/initialPwd.py
@@ -1,25 +1,9 @@
-from csv import DictReader
-from json import dump
-
-csv_file_path = "patients.csv"
-json_file_path = "test/initialPwd.json"
-initial_pwd = {}
-
-
+from bcrypt import gensalt, hashpw
 from pandas import read_csv
-from bcrypt import checkpw
-from loginA import mhash
 
-df = read_csv(csv_file_path, usecols=["Patient", "ID/Passwort"])
-df["ID/Passwort"].map(lambda pwd: mhash(pwd))
-print(df.to_string())
+original_file_path = "patients.csv"
+new_file_path = "pwd.csv"
 
-exit(1)
-
-with open(csv_file_path, "r") as filestream:
-    patients_data = DictReader(filestream)
-    for row in patients_data:   
-        initial_pwd[row["Patient"]] = row["ID/Passwort"]
-    
-with open(json_file_path, "w") as filestream:
-    dump(initial_pwd, filestream, indent=4, ensure_ascii=False)
+df = read_csv(original_file_path, usecols=["Patient", "ID/Passwort"])
+df["Hash"] = df["ID/Passwort"].map(lambda plain: hashpw(plain.encode(), gensalt()))
+df.to_csv(new_file_path, columns=["Patient", "Hash"], index=False)
