675bacd6c9751d3b6541619031499b6d6ddab246
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-11-03 09:31:42 +0100

    generaliesed new file path to just the directory

--- source/parsingExcelData.py
+++ source/parsingExcelData.py
@@ -2,20 +2,20 @@ from pandas import read_excel, read_csv
 from bcrypt import hashpw, gensalt
 
 file_path = "source/data.xlsx"
-new_file_path = "source/pwd.csv"
+new_file_directory_path = "source"
 
 # parsing the inital data excel file to csv files
 # parsing the patients table
 df_patients = read_excel(io=file_path, sheet_name="Stamm-Patienten", header=3, usecols="C:G")
-df_patients.to_csv(path_or_buf="source/patients.csv", index=False)
+df_patients.to_csv(path_or_buf=f"{new_file_directory_path}/patients.csv", index=False)
 
 # hashing passwords
 df_patients["Hash"] = df_patients["ID/Passwort"].map(lambda plain: hashpw(plain.encode(), gensalt()).decode())
-df_patients.to_csv(path_or_buf=new_file_path, columns=["Patient", "Hash"], index=False)
+df_patients.to_csv(path_or_buf=f"{new_file_directory_path}/pwd.csv", columns=["Patient", "Hash"], index=False)
 
 # parsing the doctors table
 df_doctors = read_excel(io=file_path, sheet_name="Zahnärzte", header=2, usecols="B:E")
-df_doctors.to_csv(path_or_buf="source/doctors.csv", index=False)
+df_doctors.to_csv(path_or_buf=f"{new_file_directory_path}/doctors.csv", index=False)
 
 # parsing the costs table
 df_costs = read_excel(io=file_path, sheet_name="Kosten und Behandlungsdauer", header=3, usecols="B:G")
@@ -35,4 +35,4 @@ for index, is_nan in enumerate(df_costs["Dentale Problematik"].isna()):
         #   ansatz: df_costs.loc[:, ("Dentale Problematik", index)] = dental_problem
         df_costs["Dentale Problematik"][index] = dental_problem
 
-df_costs.to_csv(path_or_buf="source/costs.csv", index=False)
+df_costs.to_csv(path_or_buf=f"{new_file_directory_path}/costs.csv", index=False)
