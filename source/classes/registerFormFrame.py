0792b81445fe3d436a01cc072d38ee477c675278
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-11-14 14:12:33 +0100

    added Tabs to Register

--- source/classes/registerFormFrame.py
+++ source/classes/registerFormFrame.py
@@ -4,40 +4,51 @@ import customtkinter as ctk
 from source.classes.customWidgets.intSpinbox import IntSpinbox
 
 
-class RegisterFormFrame(ctk.CTkFrame):
+class RegisterFormFrame(ctk.CTkTabview):
     def __init__(self, master):
         super().__init__(master=master)
         
+        # tabs
+        self.add("als Patient")
+        self.add("als Zahnarzt")
+        
+        # variables
         self.input_width = 160
         self.auth_service = self.nametowidget(".").auth_service
 
+        # font
         self.font15 = ctk.CTkFont(family="Segoe UI", size=15)
 
+        # tk variables
+        # patient tab
         self.insurance_var = tk.StringVar(value="Krankenkassenart")
         self.dental_problem_var = tk.StringVar(value="Dentale Problematik")
         self.teeth_count_var = tk.IntVar()
 
         self.insurance_types = ["gesetzlich", "freiwillig gesetzlich", "privat"]
         self.dental_problem_types = ["Karies klein", "Karies groß", "Teilkrone", "Krone", "Wurzelbehandlung"]
+        # doctor tab
+        
 
-        self.heading = ctk.CTkLabel(self, font=self.font15, text="Bitte hier registrieren")
-        self.username_entry = ctk.CTkEntry(self, width=self.input_width, placeholder_text="Benutzername")
-        self.password_entry = ctk.CTkEntry(self, width=self.input_width, placeholder_text="Passwort", show="•")
-        self.confirm_password_entry = ctk.CTkEntry(self, width=self.input_width, placeholder_text="Passwort bestätigen", show="•")
+        # widgets
+        self.heading = ctk.CTkLabel(self.tab("als Patient"), font=self.font15, text="Bitte hier registrieren")
+        self.username_entry = ctk.CTkEntry(self.tab("als Patient"), width=self.input_width, placeholder_text="Benutzername")
+        self.password_entry = ctk.CTkEntry(self.tab("als Patient"), width=self.input_width, placeholder_text="Passwort", show="•")
+        self.confirm_password_entry = ctk.CTkEntry(self.tab("als Patient"), width=self.input_width, placeholder_text="Passwort bestätigen", show="•")
 
         self.insurance_combobox = ctk.CTkComboBox(
-            self, width=self.input_width, values=self.insurance_types, variable=self.insurance_var, state="readonly")
+            self.tab("als Patient"), width=self.input_width, values=self.insurance_types, variable=self.insurance_var, state="readonly")
         self.dental_problem_combobox = ctk.CTkComboBox(
-            self, width=self.input_width, values=self.dental_problem_types, variable=self.dental_problem_var, state="readonly")
+            self.tab("als Patient"), width=self.input_width, values=self.dental_problem_types, variable=self.dental_problem_var, state="readonly")
 
-        self.teeth_count_label = ctk.CTkLabel(self, text="Anzahl zu behandelnder Zähne")
-        self.teeth_count_spinbox = IntSpinbox(self, width=self.input_width)
-        self.register_button = ctk.CTkButton(self, text="Registrieren", command=self.try_register)
+        self.teeth_count_label = ctk.CTkLabel(self.tab("als Patient"), text="Anzahl zu behandelnder Zähne")
+        self.teeth_count_spinbox = IntSpinbox(self.tab("als Patient"), width=self.input_width)
+        self.register_button = ctk.CTkButton(self.tab("als Patient"), text="Registrieren", command=self.try_register)
 
         self.set_grid()
 
     def set_grid(self):
-        self.heading.grid(row=1, column=0, pady=(20, 0), padx=0, sticky="n")
+        self.heading.grid(row=1, column=0, pady=(10, 0), padx=0, sticky="n")
         self.username_entry.grid(row=2, column=0, pady=(20, 0), padx=50, sticky="n")
         self.password_entry.grid(row=3, column=0, pady=(15, 0), padx=50, sticky="n")
         self.confirm_password_entry.grid(row=4, column=0, pady=(10, 0), padx=50, sticky="n")
@@ -45,7 +56,7 @@ class RegisterFormFrame(ctk.CTkFrame):
         self.dental_problem_combobox.grid(row=6, column=0, pady=(15, 0), padx=50, sticky="n")
         self.teeth_count_label.grid(row=7, column=0, pady=(10, 0), padx=50, sticky="n")
         self.teeth_count_spinbox.grid(row=8, column=0, pady=(2, 0), padx=50, sticky="n")
-        self.register_button.grid(row=9, column=0, pady=(25, 30), padx=50, sticky="n")
+        self.register_button.grid(row=9, column=0, pady=(25, 20), padx=50, sticky="n")
 
     def try_register(self, event=None) -> None:
         username = self.username_entry.get()
@@ -99,5 +110,5 @@ class RegisterFormFrame(ctk.CTkFrame):
         # automatically log in
         self.nametowidget(".!ctkframe2.!canvas.!mainregisterframe").grid_forget()
         self.nametowidget(".").login_sidebar.grid_forget()
-        self.nametowidget(".").main_grid(username)
+        self.nametowidget(".").main_grid()
         
