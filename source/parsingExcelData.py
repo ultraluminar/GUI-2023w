68ebb7290f9f1044ad4275e085f181f206c31a88
Lukas Klassen <lukasklassen4243@gmail.com>
2023-11-21 14:51:03 +0100

    fixes for pandas warnings

--- source/parsingExcelData.py
+++ source/parsingExcelData.py
@@ -1,21 +1,33 @@
 from pandas import read_excel
 from json import dump
-
 from classes.authentication_service import mhash
 
+from warnings import catch_warnings, filterwarnings
+
 def behandelt_analyse(df):
+    week = ["Mo", "Di", "Mi", "Do", "Fr"]
 
     for index, row in df.iterrows():
-        behandelt = row['behandelt'].lstrip("nur ").split(" und ")
+        behandelt = row["behandelt"].lstrip("nur ").split(" und ")
 
         for art in ["privat", "gesetzlich", "freiwillig gesetzlich"]:
-            df.at[index, art] = art in behandelt
+            with catch_warnings():
+                filterwarnings(action="ignore", category=FutureWarning)
+                df.at[index, art] = art in behandelt
 
-    # df.drop('behandelt', axis=1, inplace=True)
+
+        text = row["Behandlungszeiten"].replace(":", '').replace(" Uhr", '').replace(" und", '')
+        df.at[index, "Behandlungszeiten"] = text
     return df
 
 file_path = "source/data/data.xlsx"
 new_file_directory_path = "source/data"
+
+# parsing the doctors table
+df_doctors = read_excel(io=file_path, sheet_name="Zahnärzte", header=2, usecols="B:E")
+df_doctors = behandelt_analyse(df_doctors)
+df_doctors.to_csv(path_or_buf=f"{new_file_directory_path}/doctors.csv", index=False)
+
 
 # parsing the inital data excel file to csv files
 # parsing the patients table
@@ -30,11 +42,6 @@ json_data = {item["Patient"]: item["Hash"] for item in password_dict}
 with open(f"{new_file_directory_path}/pwd.json", "w", encoding="utf-8") as filestream:
     dump(json_data, filestream, indent=4, ensure_ascii=False)
 
-# parsing the doctors table
-df_doctors = read_excel(io=file_path, sheet_name="Zahnärzte", header=2, usecols="B:E")
-df_doctors = behandelt_analyse(df_doctors)
-df_doctors.to_csv(path_or_buf=f"{new_file_directory_path}/doctors.csv", index=False)
-
 # parsing the costs table
 df_costs = read_excel(io=file_path, sheet_name="Kosten und Behandlungsdauer", header=3, usecols="B:G")
 # replacing column names that are missing in the excel file
@@ -42,15 +49,9 @@ df_costs.rename(columns={"Unnamed: 5": "gesetzlicher Anteil", "Unnamed: 6": "pri
 
 # adding the "Dentale Problematik" column in every row instead of only the first
 dental_problem = ""
-for index, is_nan in enumerate(df_costs["Dentale Problematik"].isna()):
-    if not is_nan:
-        dental_problem = df_costs["Dentale Problematik"][index]
-    else:
-        # FIXME: SettingWithCopyWarning:
-        #   A value is trying to be set on a copy of a slice from a DataFrame
-        #   See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
-        #   df_costs["Dentale Problematik"][index] = dental_problem
-        #   ansatz: df_costs.loc[:, ("Dentale Problematik", index)] = dental_problem
-        df_costs["Dentale Problematik"][index] = dental_problem
+
+for index, row in df_costs.loc[df_costs["Dentale Problematik"].notna()].iterrows():
+    df_costs.loc[[index+1, index+2], "Dentale Problematik"] = row["Dentale Problematik"]
+
 
 df_costs.to_csv(path_or_buf=f"{new_file_directory_path}/costs.csv", index=False)
