d13c50e22d5580d2bed8251a510d17e4200fb1f0
Lukas Klassen <lukasklassen4243@gmail.com>
2023-11-28 13:56:56 +0100

    new parsing output

--- source/classes/Calendar.py
+++ source/classes/Calendar.py
@@ -6,32 +6,46 @@ df_doctors = read_csv("source/data/doctors.csv")
 df_patients = read_csv("source/data/patients.csv")
 
 kuerzel = ["Mo", "Di", "Mi", "Do", "Fr"]
-tage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
+tage = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
 zeiten = range(8, 18+1)
+
+def parse_days(sub: str) -> list[str]:
+    if '-' not in sub:
+        return [tage[kuerzel.index(sub)]]
+    start, stop = [kuerzel.index(subsub) for subsub in sub.split('-')]
+    return tage[start: stop+1]
+
+def parse_hours(sub: str) -> list[int]:
+    start, stop = sub.split('-')
+    return list(range(int(start), int(stop)))
+
 def setZeiten(string: str, doc: str):
-    lis, current_days = [], []
+    current_days, current_hour = [], []
     for sub in string.split(' '):
-        if sub[0].isalpha():
-            if '-' in sub:
-                first, last = [kuerzel.index(subsub) for subsub in sub.split('-')]
-                current_days = tage[first: last+1]
-            else:
-                current_days = [tage[kuerzel.index(sub)]]
-        elif sub[0].isdigit():
-            first, last = sub.split('-')
-            current_zeiten = range(int(first), int(last)) if '-' in sub else sub
+        if sub[0].isalpha():  # wenn sub Tage darstellt
+            current_days = parse_days(sub)
+
+        elif sub[0].isdigit():  # wenn sub Stunden darstellt
+            current_hour = parse_hours(sub)
+
             for day in current_days:
-                for hour in current_zeiten:
+                for hour in current_hour:
                     data_doctors[doc][day][hour]["busy"] = False
 
 
-termin = {"Tag": "Dienstag", "Zeit": 9}
-
 data_doctors = {arzt: {day: {hour: {} for hour in zeiten} for day in tage} for arzt in df_doctors["Zahnarzt"]}
+
 for index, row in df_doctors.iterrows():
     setZeiten(row["Behandlungszeiten"], row["Zahnarzt"])
 
-with open("source/data/data_doctors.json", mode="x") as file:
+for doc, week in data_doctors.items():
+    for day, hours in week.items():
+        for hour, event in hours.items():
+            if not event:
+                print(f"{doc} hat {day}s um {hour} Uhr frei !")
+
+
+with open("source/data/data_doctors.json", mode="w") as file:
     dump(data_doctors, file, indent=4)
 
 data_patients = {patient: [] for patient in df_patients["Patient"]}
--- source/parsingExcelData.py
+++ source/parsingExcelData.py
@@ -24,46 +24,48 @@ new_file_directory_path = "source/data"
 # parsing the inital data excel file to csv files
 
 # parsing the doctors table
-print("parsing doctors...")
+print("parsing doctors... ", end='')
 df_doctors = read_excel(io=file_path, sheet_name="Zahnärzte", header=2, usecols="B:E")
 df_doctors = behandelt_analyse(df_doctors)
 df_doctors["Name"] = df_doctors["Zahnarzt"]
 df_doctors = df_doctors[["Zahnarzt", "Name", "ID/Passwort", "behandelt", "Behandlungszeiten", "privat", "gesetzlich", "freiwillig gesetzlich"]]
 df_doctors.to_csv(path_or_buf=f"{new_file_directory_path}/doctors.csv", index=False)
-print("success")
+print("done")
 
 # parsing the patients table
-print("parsing patients...")
+print("parsing patients... ", end='')
 df_patients = read_excel(io=file_path, sheet_name="Stamm-Patienten", header=3, usecols="C:G")
 df_patients["Name"] = df_patients["Patient"]
 df_patients = df_patients[["Patient", "Name", "ID/Passwort", "Krankenkassenart", "Dentale Problematik", "Anzahl zu behandelnder Zähne"]]
 df_patients.to_csv(path_or_buf=f"{new_file_directory_path}/patients.csv", index=False)
-print("success")
+print("done.")
 
 # hashing passwords
-print("hashing initial passwords...")
+print("hashing initial passwords... ", end='')
 df_patients["Hash"] = df_patients["ID/Passwort"].map(mhash)
 df_doctors["Hash"] = df_doctors["ID/Passwort"].map(mhash)
 
 for _, row in df_doctors.iterrows():
     df_patients.loc[len(df_patients), ["Patient", "Hash"]] = row["Zahnarzt"], row["Hash"]
+print("done.")
 
+print("creating json file... ", end='')
 json_data = {item["Patient"]: item["Hash"] for item in df_patients.to_dict(orient="records")}
-print("creating json file...")
 with open(f"{new_file_directory_path}/pwd.json", "w", encoding="utf-8") as filestream:
     dump(json_data, filestream, indent=4, ensure_ascii=False)
-print("success")
+print("done.")
 
 # parsing the costs table
-print("parsing costs...")
+print("parsing costs... ", end='')
 df_costs = read_excel(io=file_path, sheet_name="Kosten und Behandlungsdauer", header=3, usecols="B:G")
 # replacing column names that are missing in the excel file
 df_costs.rename(columns={"Unnamed: 5": "gesetzlicher Anteil", "Unnamed: 6": "privater Anteil"}, inplace=True)
+print("done.")
 
 # adding the "Dentale Problematik" column in every row instead of only the first
-print("adding missing dental problem column...")
+print("adding missing dental problem column... ", end='')
 for index, row in df_costs.loc[df_costs["Dentale Problematik"].notna()].iterrows():
     df_costs.loc[[index+1, index+2], "Dentale Problematik"] = row["Dentale Problematik"]
 
 df_costs.to_csv(path_or_buf=f"{new_file_directory_path}/costs.csv", index=False)
-print("success")
+print("done.")
