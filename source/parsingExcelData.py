ffda36cf2224ab151e390338e01b446adc5ebf2d
KlasL4243 <lukasklassen4243@gmail.com>
2023-12-02 22:50:28 +0100

    smol changes

--- source/parsingExcelData.py
+++ source/parsingExcelData.py
@@ -19,8 +19,9 @@ class ExcelToCSV:
     def __enter__(self):
         return self.df
 
-    def __exit__(self, *args):
-        self.df.to_csv(path_or_buf=f"{path_data}/{self.csv_name}.csv", index=False)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.df.to_csv(path_or_buf=f"{path_data}/{self.csv_name}.csv", index=False)
 
 
 def parseKrankenKassenArt(inplace: bool) -> None:
@@ -61,9 +62,9 @@ def parseDentaleProblematik(inplace: bool) -> None:
         df_costs.loc[[index + 1, index + 2], "Dentale Problematik"] = row["Dentale Problematik"]
 
 def parsePasswords(df) -> dict:
-    return {row["Name"]: mhash(row["ID/Passwort"]) for _, row in df_doctors.iterrows()}
+    return {row["Name"]: mhash(row["ID/Passwort"]) for _, row in df.iterrows()}
 
-print("parsing doctors ...")
+
 with ExcelToCSV(sheet="Zahnärzte", header=2, cols="B:E", csv_name="doctors") as df_doctors:
     parseKrankenKassenArt(inplace=True)
     behandlungszeiten = parseBehandlungsZeiten()
@@ -71,20 +72,17 @@ with ExcelToCSV(sheet="Zahnärzte", header=2, cols="B:E", csv_name="doctors") as
     password_hashes |= parsePasswords(df_doctors)
     df_doctors = df_doctors[["Name", "ID/Passwort", "behandelt", "Behandlungszeiten", "privat", "gesetzlich", "freiwillig gesetzlich"]]
 
-print("parsing patients ... ")
 with ExcelToCSV(sheet="Stamm-Patienten", header=3, cols="C:G", csv_name="patiens") as df_patients:
     df_patients.rename(columns={"Patient": "Name"}, inplace=True)
     password_hashes |= parsePasswords(df_patients)
     df_patients = df_patients[["Name", "ID/Passwort", "Krankenkassenart", "Dentale Problematik", "Anzahl zu behandelnder Zähne"]]
 
-print("parsing costs ...")
 with ExcelToCSV(sheet="Kosten und Behandlungsdauer", header=3, cols="B:G", csv_name="costs") as df_costs:
     parseDentaleProblematik(inplace=True)
     df_costs.rename(columns={"Unnamed: 5": "gesetzlicher Anteil", "Unnamed: 6": "privater Anteil"}, inplace=True)
 
-print("creating json files ...")
-with open(f"{path_data}/pwd.json", mode="w", encoding="utf-8") as filestream:
-    dump(password_hashes, filestream, indent=4, ensure_ascii=False)
+with open(f"{path_data}/pwd.json", mode="w", encoding="utf-8") as file:
+    dump(password_hashes, file, indent=4, ensure_ascii=False)
 
-with open("source/data/data_doctors.json", mode="w") as file:
+with open("source/data/data_doctors.json", mode="w", encoding="utf-8") as file:
     dump(behandlungszeiten, file, indent=4)
