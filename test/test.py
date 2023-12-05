33ad3c8515fbdd10c8a335182c24fcb55c128205
Lukas Klassen <lukasklassen4243@gmail.com>
2023-12-05 15:16:45 +0100

    smoll changes

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from dateutil.rrule import rrule, rrulestr, weekday, WEEKLY, MO, TU, WE, TH, FR, rul
+from dateutil.rrule import rrule, rrulestr, weekday, WEEKLY, MO, TU, WE, TH, FR
 from dateutil.relativedelta import relativedelta
 from datetime import datetime, time
 
@@ -10,5 +10,9 @@ woche = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
 rule = rrule(freq=WEEKLY, byweekday=(MO, TU, WE, TH, FR), byhour=(8, 9, 10, 11, 12), byminute=0, bysecond=0)
 date_list: list[datetime] = rule.between(after=today, before=in_a_week)
 
-for date in date_list:
-    print(f"am {woche[date.weekday()]:>10} um {date.time()} Uhr")
+#for date in date_list:
+#    print(f"am {woche[date.weekday()]:>10} um {date.time()} Uhr")
+
+from source.classes.authentication_service import appendJson
+
+appendJson("data/data_doctors.json", {"balls":  3})
