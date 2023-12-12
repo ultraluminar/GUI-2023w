a4d053c91d4fc8556bf176201e2d696b6b8e1b58
Lukas Klassen <lukasklassen4243@gmail.com>
2023-12-12 16:05:18 +0100

    input validation

--- source/classes/registerFormFrame.py
+++ source/classes/registerFormFrame.py
@@ -9,8 +9,6 @@ class RegisterFormFrame(ctk.CTkTabview):
     def __init__(self, master):
         super().__init__(master=master)
         
-        self.auth_service = self.nametowidget(".").auth_service
-        
         # tabs
         self.add("als Patient")
         self.add("als Zahnarzt")
@@ -60,9 +58,15 @@ class RegisterFormFrame(ctk.CTkTabview):
         # doctor tab
         self.doctor_heading = ctk.CTkLabel(self.tab("als Zahnarzt"), font=self.font15, text="Bitte hier registrieren")
         self.doctor_username_entry = ctk.CTkEntry(self.tab("als Zahnarzt"), width=self.input_width, placeholder_text="Benutzername")
+
         self.doctor_name_entry = ctk.CTkEntry(self.tab("als Zahnarzt"), width=self.input_width, placeholder_text="Nachname")
+        self.doctor_name_entry.configure(validate="key", validatecommand=(self.doctor_username_entry.register(self.isVarAlpha), '%d', '%P'))
+
         self.doctor_password_entry = ctk.CTkEntry(self.tab("als Zahnarzt"), width=self.input_width, placeholder_text="Passwort", show="•")
         self.doctor_confirm_password_entry = ctk.CTkEntry(self.tab("als Zahnarzt"), width=self.input_width, placeholder_text="Passwort bestätigen", show="•")
+
+        self.doctor_otp = ctk.CTkEntry(self.tab("als Zahnarzt"), width=self.input_width, placeholder_text="Freischalt-Code")
+        self.doctor_otp.configure(validate="key", validatecommand=(self.doctor_otp.register(self.isVarDigit), '%d', '%P'))
 
         self.doctor_adress_male = ctk.CTkRadioButton(self.tab("als Zahnarzt"), text="Herr", value="Herr", variable=self.sex)
         self.doctor_adress_female = ctk.CTkRadioButton(self.tab("als Zahnarzt"), text="Frau", value="Frau", variable=self.sex)
@@ -79,18 +83,33 @@ class RegisterFormFrame(ctk.CTkTabview):
         # gridding
         self.set_patient_grid()
         self.set_doctor_grid()
+
+    @staticmethod
+    def isVarAlpha(action_type: str, string: str):
+        if action_type != "1":
+            return True
+
+        return string.isalpha()
+
+    @staticmethod
+    def isVarDigit(action_type: str, string: str):
+        if action_type != "1":
+            return True
+        if len(string) > 6:
+            return False
+        return string.isdigit()
         
 
     def set_patient_grid(self):
         self.patient_heading.grid(row=1, column=0, columnspan=2, pady=(10, 0), padx=0, sticky="n")
         self.patient_username_entry.grid(row=2, column=0, columnspan=2, pady=(20, 0), padx=50, sticky="n")
-        self.patient_name_entry.grid(row=3, column=0, columnspan=2, pady=(10, 0), padx=50, sticky="n")
+        self.patient_name_entry.grid(row=3, column=0, columnspan=2, pady=(5, 0), padx=50, sticky="n")
         self.patient_password_entry.grid(row=4, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
-        self.patient_confirm_password_entry.grid(row=5, column=0, columnspan=2, pady=(10, 0), padx=50, sticky="n")
+        self.patient_confirm_password_entry.grid(row=5, column=0, columnspan=2, pady=(5, 0), padx=50, sticky="n")
         self.patient_adress_male.grid(row=6, column=0, pady=(20, 0), padx=(70, 0), sticky="n")
         self.patient_adress_female.grid(row=6, column=1, pady=(20, 0), padx=(0, 30), sticky="n")
         self.insurance_combobox.grid(row=7, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
-        self.dental_problem_combobox.grid(row=8, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
+        self.dental_problem_combobox.grid(row=8, column=0, columnspan=2, pady=(5, 0), padx=50, sticky="n")
         self.teeth_count_label.grid(row=9, column=0, columnspan=2, pady=(10, 0), padx=50, sticky="n")
         self.teeth_count_spinbox.grid(row=10, column=0, columnspan=2, pady=(2, 0), padx=50, sticky="n")
         self.patient_register_button.grid(row=11, column=0, columnspan=2, pady=(25, 20), padx=50, sticky="n")
@@ -98,17 +117,18 @@ class RegisterFormFrame(ctk.CTkTabview):
     def set_doctor_grid(self):
         self.doctor_heading.grid(row=1, column=0, columnspan=2, pady=(10, 0), padx=0, sticky="n")
         self.doctor_username_entry.grid(row=2, column=0, columnspan=2, pady=(20, 0), padx=50, sticky="n")
-        self.doctor_name_entry.grid(row=3, column=0, columnspan=2, pady=(10, 0), padx=50, sticky="n")
+        self.doctor_name_entry.grid(row=3, column=0, columnspan=2, pady=(5, 0), padx=50, sticky="n")
         self.doctor_password_entry.grid(row=4, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
-        self.doctor_confirm_password_entry.grid(row=5, column=0, columnspan=2, pady=(10, 0), padx=50, sticky="n")
-        self.doctor_adress_male.grid(row=6, column=0, pady=(20, 0), padx=(70, 0), sticky="n")
-        self.doctor_adress_female.grid(row=6, column=1, pady=(20, 0), padx=(0, 30), sticky="n")
-        self.doctor_insurance_label.grid(row=7, column=0, columnspan=2, pady=(15, 0), sticky="n")
-        self.doctor_insurance_checkbox_private.grid(row=8, column=0, columnspan=2, pady=(5, 0), padx=(50, 0), sticky="w")
-        self.doctor_insurance_checkbox_by_law.grid(row=9, column=0, columnspan=2, pady=(8, 0), padx=(50, 0), sticky="w")
-        self.doctor_insurance_checkbox_voluntarily.grid(row=10, column=0, columnspan=2, pady=(8, 0), padx=(50, 0), sticky="w")
-        self.doctor_time_selector_button.grid(row=11, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
-        self.doctor_register_button.grid(row=12, column=0, columnspan=2, pady=(25, 20), padx=50, sticky="n")
+        self.doctor_confirm_password_entry.grid(row=5, column=0, columnspan=2, pady=(5, 0), padx=50, sticky="n")
+        self.doctor_otp.grid(row=6, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
+        self.doctor_adress_male.grid(row=7, column=0, pady=(20, 0), padx=(70, 0), sticky="n")
+        self.doctor_adress_female.grid(row=7, column=1, pady=(20, 0), padx=(0, 30), sticky="n")
+        self.doctor_insurance_label.grid(row=8, column=0, columnspan=2, pady=(15, 0), sticky="n")
+        self.doctor_insurance_checkbox_private.grid(row=9, column=0, columnspan=2, pady=(5, 0), padx=(50, 0), sticky="w")
+        self.doctor_insurance_checkbox_by_law.grid(row=10, column=0, columnspan=2, pady=(8, 0), padx=(50, 0), sticky="w")
+        self.doctor_insurance_checkbox_voluntarily.grid(row=11, column=0, columnspan=2, pady=(8, 0), padx=(50, 0), sticky="w")
+        self.doctor_time_selector_button.grid(row=12, column=0, columnspan=2, pady=(15, 0), padx=50, sticky="n")
+        self.doctor_register_button.grid(row=13, column=0, columnspan=2, pady=(25, 20), padx=50, sticky="n")
 
     def try_patient_register(self, event=None) -> None:
         username = self.patient_username_entry.get()
@@ -186,10 +206,13 @@ class RegisterFormFrame(ctk.CTkTabview):
         address = self.sex.get()
         password = self.doctor_password_entry.get()
         confirm_password = self.doctor_confirm_password_entry.get()
+        otp = self.doctor_otp.get()
         insurance_private = self.insurance_private.get()
         insurance_by_law = self.insurance_by_law.get()
         insurance_voluntarily = self.insurance_voluntarily.get()
         insurances = [insurance_voluntarily, insurance_by_law, insurance_private]
+        otp_verified = otp != "" and not self.auth_service.check_otp(otp)
+
         
         default_color = ("#979DA2", "#565B5E")
         
@@ -201,7 +224,9 @@ class RegisterFormFrame(ctk.CTkTabview):
             [self.doctor_confirm_password_entry,     confirm_password != password,  "your confirmation password does not match"],
             [self.doctor_insurance_checkbox_private, not any(insurances),           "choose at least one type of insurance"],
             [self.doctor_time_selector_button,       self.availability == [],       "select ure availability schedule"],
-            [self.doctor_username_entry,             username_exists(username),     "username already exists"]]
+            [self.doctor_username_entry,             username_exists(username),     "username already exists"],
+            [self.doctor_otp,                        otp == "",                     "no authorization code given"],
+            [self.doctor_otp,                        otp_verified,                  "wrong authorization code"]]
         
         error_entrys = []
         for entry, is_problem, error_string in entry_map:
