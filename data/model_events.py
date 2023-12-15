542e42c45ea8bb940c5425fd4a45735048a9dc6f
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-12-15 10:57:43 +0100

    update event model optimized for multiple event on same day

--- data/model_events.py
+++ data/model_events.py
@@ -1,13 +1,12 @@
 from typing import Literal
-from datetime import time, date
+from datetime import time, datetime
 from dateutil.rrule import rrule
 
 event_model = {
     "arzt": {
         "patient": {
-            date: {
-                "start_time": time,  # 8 bis 18 Uhr
-                "end_time": time,   # 8 bis 18 Uhr
+            datetime: {
+                "t_stop": time,   # 8 bis 18 Uhr
                 "dental_problem": Literal["Karies klein", "Karies groß", "Teilkrone", "Krone", "Wurzelbehandlung"],
                 "tooth_count": int,
                 "fill_type": Literal["normal", "höherwertig", "höchstwertig"]
