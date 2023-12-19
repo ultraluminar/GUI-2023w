f183af27ae3710cb9061291d3e94a2fff6285174
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-12-19 11:43:47 +0100

    Fix typo in dental problem field name

--- data/model_events.py
+++ data/model_events.py
@@ -6,7 +6,7 @@ date: datetime = None
 t_stop: datetime = None
 
 # event_model
-header = ["doctor", "patient", date, t_stop, "dental_problema", "tooth_count", "fill_type"]
+header = ["doctor", "patient", date, t_stop, "dental_problem", "tooth_count", "fill_type"]
 
 availibility_model = {
     "arzt": [
