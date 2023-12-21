9bc900e02036f24e6e81c9874d7e5e5527a34320
Lukas Klassen <lukasklassen4243@gmail.com>
2023-12-21 11:51:14 +0100

    big changes

--- source/classes/customWidgets/calenderWeekView.py
+++ source/classes/customWidgets/calenderWeekView.py
@@ -25,48 +25,58 @@ class WeekCalenderView(ctk.CTkFrame):
         self.start_week = None
         self.stop_week = None
         self.doctor_name = None
+        self.dates = None
 
         self.day_shortnames: list[str] = ["MO", "DI", "MI", "DO", "FR"]
 
         self.grid_columnconfigure(list(range(5)), weight=1)
         self.grid_rowconfigure(list(range(2, 10*4+2)), weight=1)
 
+        self.set_week_limits(datetime.now())
 
-        self.start_date: datetime = datetime.now() + relativedelta(weekday=MO(-1), hour=0)
-        self.stop_date: datetime = self.start_date + relativedelta(weekday=SA)
-
-        self.rule = rrule(freq=WEEKLY, byweekday=range(5), byhour=8, byminute=0, bysecond=0, dtstart=self.start_date)
-        self.dates: list[str] = [str(dt.day) for dt in self.rule.between(after=self.start_date, before=self.stop_date)]
+        self.date_rule = rrule(freq=WEEKLY, byweekday=range(5), byhour=8, byminute=0, bysecond=0, dtstart=self.start_week)
 
         # widgets
         self.day_labels: list[ctk.CTkLabel] = [ctk.CTkLabel(self, text=day) for day in self.day_shortnames]
-        self.date_labels: list[ctk.CTkLabel] = [ctk.CTkLabel(self, text=day) for day in self.dates]
+        self.date_labels: list[ctk.CTkLabel] = [ctk.CTkLabel(self) for _ in workdays]
         self.hour_buttons: list[list[ctk.CTkLabel]] = [[
             ctk.CTkLabel(self, text=f"{hour} - {hour+1} Uhr", corner_radius=5, fg_color="gray20"
-                ) for hour in range(8, 18)] for _ in self.dates]
+                ) for hour in range(8, 18)] for _ in workdays]
 
+        self.update_date_labels()
         self.set_grid()
 
-    def set_week(self, day_of_week: datetime, doctor_name=None, ex_dates: list[tuple[datetime, datetime]] = None):
-        self.reset()
-
-        if doctor_name is not None:
-            self.doctor_name = doctor_name
-
+    def set_week_limits(self, day_of_week: datetime):
         self.start_week = day_of_week + relativedelta(weekday=MO(-1), hour=0)
         self.stop_week = self.start_week + relativedelta(weekday=SA)
 
+    def set_week(self, day_of_week: datetime, doctor_name=None, ex_dates: list[tuple[datetime, datetime]] = None):
+        if doctor_name is not None:
+            self.doctor_name = doctor_name
+
+        self.set_week_limits(day_of_week)
+        self.update_date_labels()
+        self.update_event_labels()
+
+    def update_event_labels(self):
+        self.reset()
+
+        rule_strings = loadJson(path_docotrs_free)[self.doctor_name]
+        rules = [rrulestr(rule) for rule in rule_strings]
+        labels = self.labels_from_rules(rules)
+
         week_filter = self.get_week_filter()
-        labels = self.labels_from_rules(self.rules_from_doc(self.doctor_name))
         labels = [label for isday, label in zip(week_filter, labels) if isday]
+
         self.events.extend(labels)
-
-        self.grid_events()
-
-    def grid_events(self):
         for label in self.events:
             label.grid()
 
+
+    def update_date_labels(self):
+        self.dates = [str(dt.day) for dt in self.date_rule.between(after=self.start_week, before=self.stop_week)]
+        for date, label in zip(self.dates, self.date_labels):
+            label.configure(text=date)
 
     def labels_from_rules(self, rules: list[rrule]):
         labels: list[EventLabel] = []
@@ -79,12 +89,6 @@ class WeekCalenderView(ctk.CTkFrame):
             labels.extend(labels)
 
         return labels
-
-    @staticmethod
-    def rules_from_doc(doctor_name: str):
-        rule_strings = loadJson(path_docotrs_free)[doctor_name]
-        return [rrulestr(rule) for rule in rule_strings]
-
 
     def get_week_filter(self) -> list[bool]:
         days: rrule = rrule(freq=WEEKLY, dtstart=self.start_week, byweekday=weekdays, byhour=8)
@@ -110,18 +114,3 @@ class WeekCalenderView(ctk.CTkFrame):
         for column, rows in enumerate(self.hour_buttons):
             for row, widget in enumerate(rows):
                 widget.grid(column=column, row=row * 4 + 2, sticky="nsew", padx=1, pady=1, rowspan=4)
-
-
-if __name__ == "__main__":
-    names = loadJson(path_docotrs_free)
-
-    root = ctk.CTk()
-    center_window(root, 400, 400)
-
-    view = WeekCalenderView(root)
-    day_of_week = datetime.now() + relativedelta(hour=0)
-    week = ["start"]
-    while any(week):
-        week = view.get_week_filter(day_of_week=day_of_week)
-        print(week, day_of_week.day)
-        day_of_week += relativedelta(weeks=1)
