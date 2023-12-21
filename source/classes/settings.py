786a66b7a8096a0173d94d44f942e08cbc5ae164
Adrian Lösch <loeschadrianp+git@gmail.com>
2023-12-21 17:32:05 +0100

    Refactor settings window layout

--- source/classes/settings.py
+++ source/classes/settings.py
@@ -6,9 +6,8 @@ class SettingsWindow(ctk.CTkToplevel):
     def __init__(self):
         super().__init__()
         
-        #variables
-        settings_width = 440
-        settings_height = 400
+        # variables
+        self.inner_width = 400
         
         # window icon
         self.iconpath = ImageTk.PhotoImage(Image.open("assets/zahn_logo_dark.png", "r"))
@@ -16,7 +15,6 @@ class SettingsWindow(ctk.CTkToplevel):
         self.after(300, lambda: self.iconphoto(False, self.iconpath))
         
         # initialize window
-        self.geometry(f"{settings_width}x{settings_height}")
         self.title("Einstellungen")
         self.resizable(False, False)
         
@@ -31,36 +29,34 @@ class SettingsWindow(ctk.CTkToplevel):
 
         # design subframe widgets
         self.design_frame = ctk.CTkFrame(self)
-        self.design_label = ctk.CTkLabel(self.design_frame, text="Farbdesign ändern", font=self.font20)
-        self.design_frame_placeholder = ctk.CTkLabel(self.design_frame, text="")
+        self.grid_rowconfigure((0, 1), weight=1)
+        self.design_label = ctk.CTkLabel(self.design_frame, text="Farbdesign ändern", font=self.font20, anchor="w", width=self.inner_width)
         self.appearance_mode_optionemenu = ctk.CTkOptionMenu(self.design_frame, values=["Light", "Dark", "System"],
                                                              command=self.change_appearance_mode_event)
         self.appearance_mode_optionemenu.set("System")
         
         # password change subframe widgets
         self.change_password_frame = ctk.CTkFrame(self)
-        self.change_password_label = ctk.CTkLabel(self.change_password_frame, text="Passwort ändern", font=self.font20)
-        self.password_frame_placeholder = ctk.CTkLabel(self.change_password_frame, text="")
+        self.grid_rowconfigure((0, 1), weight=1)
+        self.change_password_label = ctk.CTkLabel(self.change_password_frame, text="Passwort ändern", font=self.font20, anchor="w", width=self.inner_width)
         self.old_password_entry = ctk.CTkEntry(self.change_password_frame, placeholder_text="Altes Passwort", show="•")
         self.new_password_entry = ctk.CTkEntry(self.change_password_frame, placeholder_text="Neues Passwort", show="•")
         self.change_password_button = ctk.CTkButton(self.change_password_frame, text="Passwort ändern", command=self.change_password)
         
         # design subframe grid       
         self.design_label.grid(row=0, column=0, padx=20, pady=(15, 20))
-        self.design_frame_placeholder.grid(row=0, column=1, ipadx=138)
         self.appearance_mode_optionemenu.grid(row=1, column=0, columnspan=2, pady=(0, 20))
         
         # password change subframe grid
         self.change_password_label.grid(row=0, column=0, padx=20, pady=(15, 20))
-        self.password_frame_placeholder.grid(row=0, column=1, ipadx=154)
         self.old_password_entry.grid(row=1, column=0, columnspan=2, pady=(0, 10))
         self.new_password_entry.grid(row=2, column=0, columnspan=2, pady=(0, 20))
         self.change_password_button.grid(row=3, column=0, columnspan=2, pady=(0, 20))
         
         # main label and subframe packing on window
-        self.main_label.pack(pady=(10, 15))
-        self.design_frame.pack(pady=(0, 15))
-        self.change_password_frame.pack()
+        self.main_label.grid(column=0, row=0, pady=(10, 15), padx=20, sticky="nsew")
+        self.design_frame.grid(column=0, row=1, pady=(0, 15), padx=20, sticky="nsew")
+        self.change_password_frame.grid(column=0, row=2, pady=(0, 20), padx=20, sticky="nsew")
         
         # binging Return Key to password changing function
         self.bind("<Return>", self.change_password)
