582f57e76322b61c887f6d1d8b2e057d7e2410a8
Lukas Klassen <lukasklassen4243@gmail.com>
2024-01-02 10:14:30 +0100

    model changes

--- data/model_events.py
+++ data/model_events.py
@@ -1,12 +1,7 @@
-from typing import Literal
-from datetime import time, datetime
 from dateutil.rrule import rrule
 
-date: datetime = None
-t_stop: datetime = None
-
 # event_model
-header = ["doctor", "patient", date, t_stop, "dental_problem", "tooth_count", "fill_type"]
+header = ["doctor", "patient", "dt_start", "dt_stop", "dental_problem", "tooth_count", "fill_type"]
 
 availibility_model = {
     "arzt": [
